53b90a33d07face23f5766ae3e229453eaec5a18
Ebenezer Graham <ebenezergraham69@gmail.com>
2019-12-11 05:17:25 +0400

    fixed

--- src/main/java/me/ebenezergraham/honours/platform/configuration/SecurityConfig.java
+++ src/main/java/me/ebenezergraham/honours/platform/configuration/SecurityConfig.java
@@ -7,10 +7,15 @@ import me.ebenezergraham.honours.platform.security.oauth2.CustomOAuth2UserServic
 import me.ebenezergraham.honours.platform.security.oauth2.HttpCookieOAuth2AuthorizationRequestRepository;
 import me.ebenezergraham.honours.platform.security.oauth2.OAuth2AuthenticationFailureHandler;
 import me.ebenezergraham.honours.platform.security.oauth2.OAuth2AuthenticationSuccessHandler;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
@@ -23,6 +28,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 
 /**
  * @author Ebenezer Graham
@@ -96,6 +108,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .disable()
         .formLogin()
         .disable()
+        .requiresChannel()
+        .anyRequest().requiresInsecure()
+        .and()
         .httpBasic()
         .disable()
         .exceptionHandling()
@@ -128,5 +143,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     // Add our custom Token based authentication filter
     http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+  }
+
+  @Bean
+  public RestTemplate restTemplate()
+      throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+    TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+
+    SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
+        .loadTrustMaterial(null, acceptingTrustStrategy)
+        .build();
+
+    SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+
+    CloseableHttpClient httpClient = HttpClients.custom()
+        .setSSLSocketFactory(csf)
+        .build();
+
+    HttpComponentsClientHttpRequestFactory requestFactory =
+        new HttpComponentsClientHttpRequestFactory();
+
+    requestFactory.setHttpClient(httpClient);
+    RestTemplate restTemplate = new RestTemplate(requestFactory);
+    return restTemplate;
   }
 }
