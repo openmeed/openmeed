dbc031ed7fe9f2ccd21a306d7b5d2c8f5f8636e9
Ebenezer Graham <ebenezergraham69@gmail.com>
2019-12-12 00:03:34 +0400

    final

--- webapp/src/app/pages/login/login.component.ts
+++ webapp/src/app/pages/login/login.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit, SecurityContext} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {AuthenticationService} from '../../core/authentication.service';
 import {ApiService} from '../../core/api.service';
 import {DomSanitizer} from '@angular/platform-browser';
@@ -18,10 +18,26 @@ export class LoginComponent implements OnInit {
   url = `${environment.api}/oauth2/authorize/github?redirect_uri=${environment.webapp}/oauth2/redirect`
   //url = `${environment.api}/oauth2/authorize/github?redirect_uri=${environment.webapp}/index.html`
 
-  constructor() {
+  constructor(private router: Router, private activatedRoutes: ActivatedRoute) {
   }
 
   ngOnInit() {
+    alert("we are in");
+    console.log(history.state);
+    const token = this.activatedRoutes.snapshot.queryParamMap.get("token");
+    const access_token = this.activatedRoutes.snapshot.queryParamMap.get("access_token");
+    const roles = this.activatedRoutes.snapshot.queryParamMap.get("roles");
+    const username = this.activatedRoutes.snapshot.queryParamMap.get("username");
+    console.log(token,access_token,roles,username)
+    if (access_token != null) {
+      localStorage.setItem('github_access_token', access_token);
+      localStorage.setItem('access_token', token);
+      localStorage.setItem('roles', roles);
+      localStorage.setItem('username', username);
+      this.router.navigateByUrl('/dashboard')
+    } else {
+      this.router.navigateByUrl('/login')
+    }
   }
 
 }
